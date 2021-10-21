5ed2f973236ac7f9289a1eadde2b541e1a9b3cbe
NuHash <sameh.hussain@nuhash.net>
2021-10-21 14:58:18 +0100

    Update model.py

--- model.py
+++ model.py
@@ -140,7 +140,8 @@ class BrushstrokeOptimizer:
 
         content_img = content_img.resize((self.canvas_width, self.canvas_height))
         style_img = style_img.resize((self.canvas_width, self.canvas_height))
-        self.init_prob = skimage.transform.resize(self.init_prob,(next_H,next_W,),order=3)
+        if self.init_prob is not None:
+            self.init_prob = skimage.transform.resize(self.init_prob,(next_H,next_W,),order=3)
         content_img = np.array(content_img).astype(self.dtype)
         style_img = np.array(style_img).astype(self.dtype)
 
