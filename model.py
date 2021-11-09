a256de662cd967031790a0ac8eeefb5d47d77a60
NuHash <sameh.hussain@nuhash.net>
2021-11-09 20:20:57 +0000

    Update model.py

--- model.py
+++ model.py
@@ -217,7 +217,7 @@ class BrushstrokeOptimizer:
         self.color = tf.Variable(name='color', initial_value=color, dtype=self.dtype)
         self.location = tf.Variable(name='location', initial_value=location, dtype=self.dtype)
         self.width = tf.Variable(name='width', initial_value=width, dtype=self.dtype)
-        self.z_order = tf.Variable(name="z_order",initial_value=np.random.uniform(0,2,(location.shape[0],)),dtype=self.dtype)
+        self.z_order = tf.Variable(name="z_order",initial_value=np.random.uniform(0,8,(location.shape[0],)),dtype=self.dtype)
         
         self.style_img = tf.constant(name='style_img', value=self.style_img_np, dtype=self.dtype)
         self.varlist = [self.location, self.curve_s, self.curve_e, self.curve_c,self.z_order]
