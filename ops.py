39550d973ed13bfb7a3b623702d3754add665fea
NuHash <sameh.hussain@nuhash.net>
2021-11-09 17:26:43 +0000

    Update ops.py

--- ops.py
+++ ops.py
@@ -183,8 +183,8 @@ def renderer(curve_points, locations, colors, widths, z_order, H, W, K, canvas_c
     # Finally render curves on a canvas to obtain image.
     #I_NNs_B_ranking = tf.nn.softmax(100000. * (1.0 / (1e-8 + tf.reduce_min(dist_to_closest_point_on_line_segment, axis=[-1]))), axis=-1) # [H, W, N]
     
-    I_NNs_B_ranking = tf.nn.softmax(100000. * canvas_with_nearest_Bs_Z, axis=-1)#[H, W, N]
-    weighted_distance = tf.einsum('hwn,hwf->hwf', D, I_NNs_B_ranking)#[H,W,1]
+    I_NNs_B_ranking = tf.nn.softmax(100000. * canvas_with_nearest_Bs_Z, axis=-1)#[H, W, K]
+    weighted_distance = tf.sum(D*I_NNs_b_ranking,axis=-1,keepdims=True)#tf.einsum('hwn,hwn->hw', D, I_NNs_B_ranking)#[H,W,1]
     I_colors = tf.einsum('hwnf,hwn->hwf', canvas_with_nearest_Bs_colors, I_NNs_B_ranking) # [H, W, 3]
     bs = tf.einsum('hwnf,hwn->hwf', canvas_with_nearest_Bs_bs, I_NNs_B_ranking) # [H, W, 1]
     bs_mask = tf.math.sigmoid(bs - weighted_distance)
@@ -199,7 +199,8 @@ def renderer(curve_points, locations, colors, widths, z_order, H, W, K, canvas_c
             canvas = tf.random.normal(shape=I_colors.shape, dtype=dtype) * 0.1
     else:
         canvas = tf.constant(name='canvas_bg', value=canvas_color, dtype=dtype)
-
+    print(I_colors.shape.as_list())
+    print(bs_mask.shape.as_list())
     I = I_colors * bs_mask + (1 - bs_mask) * canvas
     return I
 
